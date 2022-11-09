3023980faaeba2839ea8fd5e7b15aecc4d386534
Jona Goldman <jonagoldman@gmail.com>
2022-11-09 07:44:19 -0300

    add user model

---
+++ src/Models/User.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Orvital\Auth\Models;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\MustVerifyEmail;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Notifications\Notifiable;
+
+abstract class User extends Model implements AuthenticatableContract, AuthorizableContract, MustVerifyEmailContract, CanResetPasswordContract
+{
+    use Notifiable;
+    use Authorizable;
+    use Authenticatable;
+    use MustVerifyEmail;
+    use CanResetPassword;
+}

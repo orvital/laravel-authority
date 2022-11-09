c9eb771f4c0537522bd20d969f96bcfc5d72c44f
Jona Goldman <jonagoldman@gmail.com>
2022-11-09 08:27:35 -0300

    add traits and notifications

--- src/Models/User.php
+++ src/Models/User.php
@@ -3,8 +3,6 @@
 namespace Orvital\Auth\Models;
 
 use Illuminate\Auth\Authenticatable;
-use Illuminate\Auth\MustVerifyEmail;
-use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
@@ -12,6 +10,8 @@ use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Notifications\Notifiable;
+use Orvital\Auth\Traits\CanResetPassword;
+use Orvital\Auth\Traits\MustVerifyEmail;
 
 abstract class User extends Model implements AuthenticatableContract, AuthorizableContract, MustVerifyEmailContract, CanResetPasswordContract
 {

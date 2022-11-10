c1b4d81a33bbb76efb8d8eab371beb858e7cf6ea
Jona Goldman <jonagoldman@gmail.com>
2022-11-10 00:27:26 -0300

    fix facade consts

--- src/Invites/Facades/Invite.php
+++ src/Invites/Facades/Invite.php
@@ -3,12 +3,38 @@
 namespace Orvital\Auth\Invites\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Orvital\Auth\Invites\Contracts\InviteBroker;
 
 /**
  * @mixin \Orvital\Auth\Invites\InviteBroker
  */
 class Invite extends Facade
 {
+    /**
+     * Constant representing a successfully invite sent.
+     */
+    public const INVITE_SENT = InviteBroker::INVITE_SENT;
+
+    /**
+     * Constant representing a successfully invite accepted.
+     */
+    public const INVITE_ACCEPTED = InviteBroker::INVITE_ACCEPTED;
+
+    /**
+     * Constant representing the user not found response.
+     */
+    public const INVALID_USER = InviteBroker::INVALID_USER;
+
+    /**
+     * Constant representing an invalid token.
+     */
+    public const INVALID_TOKEN = InviteBroker::INVALID_TOKEN;
+
+    /**
+     * Constant representing a throttled attempt.
+     */
+    public const RESET_THROTTLED = InviteBroker::RESET_THROTTLED;
+
     /**
      * Return the registered name of the service container binding.
      * Static method calls to this class will resolve the binding from the

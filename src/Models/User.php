834daea6d2861595f3150dbb8e3bc797c69d8608
Jona Goldman <jonagoldman@gmail.com>
2022-11-10 01:25:00 -0300

    use invites in user model

--- src/Models/User.php
+++ src/Models/User.php
@@ -10,13 +10,16 @@ use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Notifications\Notifiable;
+use Orvital\Auth\Invites\Contracts\CanBeInvited as CanBeInvitedContract;
+use Orvital\Auth\Invites\Traits\CanBeInvited;
 use Orvital\Auth\Passwords\Traits\CanResetPassword;
 use Orvital\Auth\Traits\MustVerifyEmail;
 
-abstract class User extends Model implements AuthenticatableContract, AuthorizableContract, MustVerifyEmailContract, CanResetPasswordContract
+abstract class User extends Model implements AuthenticatableContract, AuthorizableContract, MustVerifyEmailContract, CanResetPasswordContract, CanBeInvitedContract
 {
     use Notifiable;
     use Authorizable;
+    use CanBeInvited;
     use Authenticatable;
     use MustVerifyEmail;
     use CanResetPassword;

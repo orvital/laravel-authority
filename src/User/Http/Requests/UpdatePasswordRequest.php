0a0756a5ab016ec1bbc5cc8c0fc58cc413287ccc
Jona Goldman <jonagoldman@gmail.com>
2022-11-15 02:52:46 -0300

    fix

--- src/User/Http/Requests/UpdatePasswordRequest.php
+++ src/User/Http/Requests/UpdatePasswordRequest.php
@@ -3,6 +3,7 @@
 namespace Orvital\Authority\User\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password as PasswordRule;
 
 class UpdatePasswordRequest extends FormRequest

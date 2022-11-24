1bf9e6a86c2076c9e31bfe54984723bbb2cf433f
Jona Goldman <jonagoldman@gmail.com>
2022-11-24 05:05:58 -0300

    RequirePassword middleware

---
+++ src/Http/Middleware/RequirePassword.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Orvital\Authority\Http\Middleware;
+
+use Illuminate\Auth\Middleware\RequirePassword as Middleware;
+
+class RequirePassword extends Middleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $redirectToRoute
+     * @param  int|null  $passwordTimeoutSeconds
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $redirectToRoute = null, $passwordTimeoutSeconds = null)
+    {
+        if ($this->shouldConfirmPassword($request, $passwordTimeoutSeconds)) {
+            if ($request->expectsJson()) {
+                return $this->responseFactory->json([
+                    'message' => 'Password confirmation required.',
+                ], 423);
+            }
+
+            return $this->responseFactory->redirectGuest(
+                $this->urlGenerator->route($redirectToRoute ?? 'password.confirm')
+            );
+        }
+
+        return $next($request);
+    }
+}

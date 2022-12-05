6348d687f3c7f1603ddbb7d6905078d1c9587979
Jona Goldman <jonagoldman@gmail.com>
2022-12-05 04:48:03 -0300

    merge api token endpoint

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use Orvital\Authority\Http\Controllers\ApiTokenController;
-use Orvital\Authority\Http\Controllers\TokenController;
 
 $middleware = [
     'auth' => implode(':', array_filter(['auth', config('authority.api.guard')])),
     'guest' => implode(':', array_filter(['guest', config('authority.api.guard')])),
 ];
 
-Route::post('token', [TokenController::class, 'store']);
+Route::post('token', [ApiTokenController::class, 'store']);
 
 Route::middleware($middleware['auth'])->group(function () {
     Route::controller(ApiTokenController::class)->group(function () {
--- src/Http/Controllers/ApiTokenController.php
+++ src/Http/Controllers/ApiTokenController.php
@@ -4,13 +4,24 @@ namespace Orvital\Authority\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
-use Orvital\Authority\Http\Requests\ApiTokenRequest;
+use Orvital\Authority\Http\Requests\TokenCreateRequest;
 
 class ApiTokenController extends Controller
 {
     public function show(Request $request)
     {
         return $request->user()->currentAccessToken();
+    }
+
+    public function store(TokenCreateRequest $request)
+    {
+        $token = $request->createToken();
+
+        // return back()->with('status', explode('|', $token->plainTextToken, 2)[1]);
+
+        return response([
+            'token' => $token->plainTextToken,
+        ]);
     }
 
     public function destroy(Request $request)
